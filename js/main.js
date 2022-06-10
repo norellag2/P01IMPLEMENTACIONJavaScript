89a2b426dcab99e7c4ad1cfda55ec6294f7c9e44
norellag2 <norellag2@gmail.com>
2022-06-10 11:32:39 +0200

    IMPLEMENTACIONJavaScript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Sintaxis básica de JavaScript 
+/*Comentarios en
+bloque*/
+// Características principales
+
+// - Ignora los espacios en blanco (a partir del primero)
+// - Lenguaje de tipo case-sensitive (distingue mayúsculas de minúsculas)
+//- Las instrucciones de código finalizan en ; (opcional)
+
+console.log('¡Hola Mundo!');
